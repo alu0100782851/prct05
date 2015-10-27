322377a5c05e8d9710dabd39e9498479c422f2c2
alu0100782851 <alu0100782851@ull.edu.es>
2015-10-27 16:19:25 +0000

    Añadida funcion para restar numeros fraccionarios

--- racional.rb
+++ racional.rb
@@ -23,36 +23,42 @@ class Fraccion
 
 	#Método que calcula la MULTIPLICACION
 	def *(dato)
-		
 		resultado = Fraccion.new(@num*dato.num,@denom*dato.denom)		 
 		simplificar(resultado)
-        
 	end
 	
 	
 	#Método que calcula la DIVISION
 	def /(dato)
-
 		resultado = Fraccion.new(@num*dato.denom,@denom*dato.num)
 		simplificar(resultado)
-        
 	end
 	
 	
 	#Metodo que calcula la SUMA
 	def +(dato)  
+		
 		aux = mcm(@denom, dato.denom)			  											#mcm de los denominadores
-		resultado = Fraccion.new(((aux/@denom)*@num)+((aux/dato.denom)*dato.num),aux) 	#suma de los objetos
-		simplificar(resultado)				  											#simplificación del resultado
+		resultado = Fraccion.new(((aux/@denom)*@num)+((aux/dato.denom)*dato.num),aux) 		#suma de los objetos
+		simplificar(resultado)				  												#simplificación del resultado
+		
 	end	
+	
+	
+	#Metodo que calcula la RESTA	
+	def -(dato)  
+		
+		aux = mcm(@denom, dato.denom)			  													#mcm de los denominadores
+		resultado = Fraccion.new(((aux/@denom)*@num)-((aux/dato.denom)*dato.num),aux) 			#resta de los objetos
+		simplificar(resultado)
+			
+	end
 	
 	
 	#Método que simplifica un racional
 	def simplificar(dato) 	
-		
 		aux = gcd(dato.num, dato.denom)					#Obtiene el mcd entre el numerador y denominador indicados a traves del objeto dato 			
 		Fraccion.new(dato.num/aux, dato.denom/aux)		#Crea un nuevo objeto del tipo fraccion que contiene la fraccion simplificada
-		
 	end
 	
 end
